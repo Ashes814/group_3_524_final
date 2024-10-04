8e911b48c62869ab939740bbfe9833d186b61dee
Ashes814 <1534661514@qq.com>
2024-10-04 14:02:17 +0800

    update basic data visualiztion of deck

--- dash_frontend/dash_frontend/src/App.tsx
+++ dash_frontend/dash_frontend/src/App.tsx
@@ -1,72 +1,151 @@
 import { useState, useEffect } from "react";
 import mapboxgl from "mapbox-gl";
 import MapContext from "./store/map-context";
-import { Scene, Mapbox } from "@antv/l7";
+import {createRoot} from 'react-dom/client';
+import {Map} from 'react-map-gl/maplibre';
+import {AmbientLight, PointLight, LightingEffect} from '@deck.gl/core';
+import {HexagonLayer} from '@deck.gl/aggregation-layers';
+import DeckGL from '@deck.gl/react';
+import {CSVLoader} from '@loaders.gl/csv';
+import {load} from '@loaders.gl/core';
 import Header from "./components/Header";
 import HomeView from "./views/HomeView";
 import "mapbox-gl/dist/mapbox-gl.css";
 
+import type {Color, PickingInfo, MapViewState} from '@deck.gl/core';
+
 import {ConfigProvider} from 'antd';
 import "./App.css";
 
+import axios from 'axios';
 
-function App(): JSX.Element {
-  const [map, setMap] = useState<mapboxgl.Map | null>(null);
-  const [scene, setScene] = useState<Scene | null>(null);
+
+// Source data CSV
+const DATA_URL =
+  'https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/3d-heatmap/heatmap-data.csv'; // eslint-disable-line
+
+const ambientLight = new AmbientLight({
+  color: [255, 255, 255],
+  intensity: 1.0
+});
+
+const pointLight1 = new PointLight({
+  color: [255, 255, 255],
+  intensity: 0.8,
+  position: [-0.144528, 49.739968, 80000]
+});
+
+const pointLight2 = new PointLight({
+  color: [255, 255, 255],
+  intensity: 0.8,
+  position: [-3.807751, 54.104682, 8000]
+});
+
+const lightingEffect = new LightingEffect({ambientLight, pointLight1, pointLight2});
+
+const INITIAL_VIEW_STATE: MapViewState = {
+  longitude: -1.415727,
+  latitude: 52.232395,
+  zoom: 6.6,
+  minZoom: 5,
+  maxZoom: 15,
+  pitch: 40.5,
+  bearing: -27
+};
+
+const MAP_STYLE = 'https://basemaps.cartocdn.com/gl/dark-matter-nolabels-gl-style/style.json';
+
+export const colorRange: Color[] = [
+  [1, 152, 189],
+  [73, 227, 206],
+  [216, 254, 181],
+  [254, 237, 177],
+  [254, 173, 84],
+  [209, 55, 78]
+];
+
+function getTooltip({object}: PickingInfo) {
+  if (!object) {
+    return null;
+  }
+  const lat = object.position[1];
+  const lng = object.position[0];
+  const count = object.points.length;
+
+  return `\
+    latitude: ${Number.isFinite(lat) ? lat.toFixed(6) : ''}
+    longitude: ${Number.isFinite(lng) ? lng.toFixed(6) : ''}
+    ${count} Accidents`;
+}
+
+type DataPoint = [longitude: number, latitude: number];
+
+
+
+
+function App({
+  mapStyle = MAP_STYLE,
+  radius = 1000,
+  upperPercentile = 100,
+  coverage = 1
+}: {
+  mapStyle?: string;
+  radius?: number;
+  upperPercentile?: number;
+  coverage?: number;
+}): JSX.Element {
+
+
+  const [data, setData] = useState([]);
+
   useEffect(() => {
-    // Initial Mapbox Token
-    const token: string = "pk.eyJ1IjoiYXNoZXMxMjMiLCJhIjoiY20xYThmMmhmMW5yazJ2czhyc3hzNms1ayJ9.OKIjDhcqf28oPqu7XnMKog";
-    mapboxgl.accessToken = token;
+    const fetchData = async () => {
 
-    // Initial Mapbox map object & Antv L7 Scene Object
-    const map = new mapboxgl.Map({
-      container: "map",
-      style: "mapbox://styles/mapbox/dark-v11",
-      center: [-1, 53],
-      zoom: 6.5,
-      projection: "mercator",
-    });
-    const scene = new Scene({
-      id: "map",
-      map: new Mapbox({
-        mapInstance: map,
-      }),
-      logoVisible: false,
-    });
+        const response = await axios.get(DATA_URL);
+        const csvData = response.data.split('\n').slice(1); // Skip the header
+        const parsedData = csvData.map(row => {
+          const [lng, lat] = row.split(',');
+          return { lng: parseFloat(lng), lat: parseFloat(lat) };
+        });
+        setData(parsedData);
+    };
 
-    // Updating China Boundary
-    map.on("style.load", () => {
-      map.setFog({});
-      // 中华疆土，一寸不让
-      map.setFilter("admin-0-boundary-disputed", [
-        "all",
-        ["==", ["get", "disputed"], "true"],
-        ["==", ["get", "admin_level"], 0],
-        ["==", ["get", "maritime"], "false"],
-        ["match", ["get", "worldview"], ["all", "CN"], true, false],
-      ]);
-      map.setFilter("admin-0-boundary", [
-        "all",
-        ["==", ["get", "admin_level"], 0],
-        ["==", ["get", "disputed"], "false"],
-        ["==", ["get", "maritime"], "false"],
-        ["match", ["get", "worldview"], ["all", "CN"], true, false],
-      ]);
-      map.setFilter("admin-0-boundary-bg", [
-        "all",
-        ["==", ["get", "admin_level"], 0],
-        ["==", ["get", "maritime"], "false"],
-        ["match", ["get", "worldview"], ["all", "CN"], true, false],
-      ]);
-    });
-
-    // Setting map and scene state
-    setMap(map);
-    setScene(scene);
+    fetchData();
   }, []);
 
+
+  const points: DataPoint[] = data.map(d => [d.lng, d.lat]);
+
+
+  const layers = [
+    new HexagonLayer<DataPoint>({
+      id: 'heatmap',
+      colorRange,
+      coverage,
+      data:points,
+      elevationRange: [0, 3000],
+      elevationScale: data && data.length ? 50 : 0,
+      extruded: true,
+      getPosition: d => d,
+      pickable: true,
+      radius,
+      upperPercentile,
+      material: {
+        ambient: 0.64,
+        diffuse: 0.6,
+        shininess: 32,
+        specularColor: [51, 51, 51]
+      },
+
+      transitions: {
+        elevationScale: 4000
+      }
+    })
+  ];
+
+
   return (
-    <MapContext.Provider value={{ map, scene }}>
+
       <ConfigProvider
         theme={{
           token: {
@@ -82,10 +161,22 @@ function App(): JSX.Element {
       <Header />
       {/* <Focus /> */}
       <HomeView />
-      <div id="map"></div>
+      <DeckGL
+      layers={layers}
+      effects={[lightingEffect]}
+      initialViewState={INITIAL_VIEW_STATE}
+      controller={true}
+      getTooltip={getTooltip}
+    >
+      <Map reuseMaps mapStyle={mapStyle} />
+    </DeckGL>
       </ConfigProvider>
-    </MapContext.Provider>
   );
 }
 
+
+
+
 export default App;
+
+
